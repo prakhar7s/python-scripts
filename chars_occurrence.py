8c64446b59bbfa9c02f917211172d7921186fef0
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-11-21 21:25:50 +0530

    Occurrences of each character

---
+++ chars_occurrence.py
@@ -0,0 +1,14 @@
+#  Please write a program which count and print the numbers of each character in a
+# string input by console.
+
+inp_str = input("enter string: ")
+unique_chars = sorted(set(inp_str))
+for ch in unique_chars:
+    print(ch, inp_str.count(ch))
+
+# using dictionary    
+# inp_str = input("enter string: ")
+# ans = {}
+# for ch in inp_str:
+#     ans[ch]=ans.get(ch,0)+1
+# print(ans)
