4ac97ca8f224939c160bcd2044f40cc6ea5b47c6
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-11-21 21:29:38 +0530

    delete file

--- remove_element.py
+++
@@ -1,6 +0,0 @@
-# By using list comprehension, please write a program to print the list after
-# removing the value 24 in [12,24,35,24,88,120,155].
-
-inp_list = [x for x in map(int, input("list: ").split())]
-rem_24 = [x for x in inp_list if x != 24]
-print(rem_24)
