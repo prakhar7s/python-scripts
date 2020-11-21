eb17e1004e615ff64c3fb41266451f1ea162ca69
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-11-21 21:34:47 +0530

    maximum of 3 numbers

---
+++ max3.py
@@ -0,0 +1,13 @@
+# WAP to find the maximum from three numbers.
+
+num1, num2, num3 = map(int, input().split())
+if num1>num2:
+    if num1>num3:
+        print(num1)
+    else:
+        print(num3)
+else:
+    if num2>num3:
+        print(num2)
+    else:
+        print(num3)
