4804a927c9def969bb6a20c07082d227e1860253
prakhar7s <56044776+prakhar7s@users.noreply.github.com>
2020-11-21 21:33:40 +0530

    maximum of 2 numbers

---
+++ max_2.py
@@ -0,0 +1,13 @@
+# WAP to find the maximum from two numbers.
+
+num1, num2 = map(int, input().split())
+
+print(num1 if num1 > num2 else num2)
+
+# OR
+# if(num1>num2):
+#     print(num1)
+# else:
+#     print(num2)
+
+
